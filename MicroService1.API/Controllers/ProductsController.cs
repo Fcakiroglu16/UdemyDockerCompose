cdb83c372dbb8583b908aa6f37ef5cee0d9a7ff1
Fatih Çakıroğlu <Fatih.Cakiroglu.dn@aygaz.com.tr>
2023-07-24 01:26:20 +0300

    8. kısım / 9. kısım / SqlServer 1. Kısım / SqlServer 2. Kısım

---
+++ MicroService1.API/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using MicroService1.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroService1.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductsController : ControllerBase
+	{
+
+		private readonly AppDbContext _context;
+
+		public ProductsController(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		[HttpPost]
+		public IActionResult Save()
+		{
+
+			_context.Products.Add(new Product() { Name = "Pen 1" });
+			_context.SaveChanges();
+			return Ok();
+
+
+
+		}
+	}
+}
---
+++ MicroService1.API/Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MicroService1.API.Models
+{
+	public class AppDbContext:DbContext
+	{
+
+		public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+		{
+			
+		}
+
+		public DbSet<Product> Products { get; set; }
+
+	}
+}
